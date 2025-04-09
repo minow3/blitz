417769d16a4f27f15226092ff700096409f4a48b
minow3 <114686250+minow3@users.noreply.github.com>
2025-04-09 10:14:16 +0100

    created functions for shuffle deck, deal Cards, array for cards

--- game.cpp
+++ game.cpp
@@ -2,6 +2,46 @@
 
 using namespace std;
 
+
+//Shuffle deck
+void shuffleDeck(vector<string>& deck) {
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(deck.begin(), deck.end(), g);
+}
+
+// Function to deal three cards per player per round
+void dealCards(vector<string>& deck) {
+    int player = 1;  // Track player number
+
+    while (deck.size() >= 6) {  // Each round needs 6 cards (3 per player)
+        cout << "Round starts:" << endl;
+
+        for (int i = 0; i < 2; ++i) {  // Loop for 2 players
+            cout << "Player " << (i + 1) << " gets: ";
+            for (int j = 0; j < 3; ++j) {  
+                cout << deck.back() << " ";  // Deal from the back of the deck
+                deck.pop_back();  // Remove the card from the deck
+            }
+            cout << endl;
+        }
+        cout << "Remaining cards in deck: " << deck.size() << endl;
+        cout << "-----------------------------" << endl;
+    }
+
+    // If any cards are left but not enough for another round
+    if (!deck.empty()) {
+        cout << "Final remaining cards (less than 6): ";
+        for (const auto& card : deck) {
+            cout << card << " ";
+        }
+        cout << endl;
+        deck.clear();  // Empty the deck
+    }
+
+    cout << "Game over! No more cards left in the deck." << endl;
+}
+
 void Play() {
     cout << "Play" << endl;
     
@@ -51,6 +91,19 @@ void cardDeck() {
 int main() {
 
     char choice;
+
+     vector<string> cards = {
+        "AC", "AD", "AH", "AS", "KC", "KD", "KH", "KS",
+        "QC", "QD", "QH", "QS", "JC", "JD", "JH", "JS",
+        "10C", "10D", "10H", "10S", "9C", "9D", "9H", "9S",
+        "8C", "8D", "8H", "8S", "7C", "7D", "7H", "7S",
+        "6C", "6D", "6H", "6S", "5C", "5D", "5H", "5S",
+        "4C", "4D", "4H", "4S", "3C", "3D", "3H", "3S",
+        "2C", "2D", "2H", "2S"
+    };
+
+    shuffleDeck(cards);
+    dealCards(cards);
 
     while(true) {
         cout << "Let's play Blitz" << endl;
