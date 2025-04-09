9fcec3dfeb61a603fe1d137c74405bfe717d2d4a
minow3 <114686250+minow3@users.noreply.github.com>
2025-04-09 10:26:21 +0100

    moving card deck into function

--- game.cpp
+++ game.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+//Card deck
+vector<string> cardDeck() {
+    return {
+        "AC", "AD", "AH", "AS", "KC", "KD", "KH", "KS",
+        "QC", "QD", "QH", "QS", "JC", "JD", "JH", "JS",
+        "10C", "10D", "10H", "10S", "9C", "9D", "9H", "9S",
+        "8C", "8D", "8H", "8S", "7C", "7D", "7H", "7S",
+        "6C", "6D", "6H", "6S", "5C", "5D", "5H", "5S",
+        "4C", "4D", "4H", "4S", "3C", "3D", "3H", "3S",
+        "2C", "2D", "2H", "2S"
+    };
+}
+
 
 //Shuffle deck
 void shuffleDeck(vector<string>& deck) {
@@ -93,16 +106,6 @@ void cardDeck() {
 int main() {
 
     char choice;
-
-     vector<string> cards = {
-        "AC", "AD", "AH", "AS", "KC", "KD", "KH", "KS",
-        "QC", "QD", "QH", "QS", "JC", "JD", "JH", "JS",
-        "10C", "10D", "10H", "10S", "9C", "9D", "9H", "9S",
-        "8C", "8D", "8H", "8S", "7C", "7D", "7H", "7S",
-        "6C", "6D", "6H", "6S", "5C", "5D", "5H", "5S",
-        "4C", "4D", "4H", "4S", "3C", "3D", "3H", "3S",
-        "2C", "2D", "2H", "2S"
-    };
 
     while(true) {
         cout << "Let's play Blitz" << endl;
